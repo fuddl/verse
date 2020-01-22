4a33077557af3925b23d4f52404eb0a013269a68
A Mueller <a.mueller@loom.de>
2020-01-22 14:21:47 +0100

    fix

--- src/pages/radial.js
+++ src/pages/radial.js
@@ -1,34 +1,37 @@
 import React from "react"
 import RadialGrid from '../components/radial-grid.js'
 
-function evenGrid(cx, cy, rings, divisions, minR) {
+function evenGrid(cx, cy, rings, divisions, area) {
   let output = [];
   let last = {};
 
 
-  for (var i = 0; i <= rings + 1; i++) {
-    let offset = last.a ? last.a : -1;
-    let r = Math.sqrt(((minR * (i * divisions) + offset)) / Math.PI);
+  for (var i = 1; i <= rings + 1; i++) {
+    let offset = last.a ? last.a : 0;
+    let r = Math.sqrt(((area * (i * divisions) + offset)) / Math.PI);
     let a = Math.PI * Math.pow(r, 2);
 
-    output.push(
-      <RadialGrid
-        cx={ cx } 
-        cy={ cy } 
-        minR={ last.r ? last.r : minR }
-        r={ r }
-        radialSubdivisions={ divisions * i }
-        stroke="black"
-        stroke-width={ 1 }
-      />
-    )
+    console.log({r: r});
+    if (r > 0) {
+      output.push(
+        <RadialGrid
+          cx={ cx } 
+          cy={ cy } 
+          minR={ last.r ? last.r : 0 }
+          r={ r }
+          radialSubdivisions={ divisions * i }
+          stroke="black"
+          stroke-width={ 1 }
+        />
+      )
+    }
     last.r = r;
     last.a = a;
   }
   return output;
 }
 
-let test = evenGrid(0, 0, 100, 6, 1);
+let test = evenGrid(0, 0, 10, 1, 1);
 
 console.log(test);
 
